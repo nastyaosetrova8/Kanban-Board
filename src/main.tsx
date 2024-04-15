d68caff59b9bcada2599dcbb36656cd748b30822
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-04-15 17:27:57 +0200

    fix in main.tsx, readme

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import React from "react";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -7,11 +8,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./config/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </React.StrictMode>
 );
