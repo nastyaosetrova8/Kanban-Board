1b5176386064a99b8dedf2a60bd2e877f8706ceb
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-04-16 03:08:15 +0200

    add github folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import IssuesPage from "./Pages/IssuesPage";
 
 function App() {
   return (
-    <>
+    <div>
       <IssuesPage />
-    </>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom/client";
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
